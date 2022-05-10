13af22cb07674a7b4297956be9251b5d35d84043
Mamytov6 <mamytov03@internet.ru>
2022-05-10 16:59:07 +0600

    sdfs

--- src/component/Product.js
+++ src/component/Product.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useDispatch, useSelector} from "react-redux";
+import Card from "./card";
+import {getProducts} from "../redux/actions";
 
 const Product = () => {
+    const dispatch = useDispatch()
+    const {products} = useSelector(s => s)
+
+    useEffect(() => {
+        dispatch(getProducts())
+    }, [])
     return (
         <div>
-
+            <div className="flex flex-row flex-wrap  ">
+                {
+                    products.map(el => (
+                        <Card product={el} key={el.id}/>
+                    ))
+                }
+            </div>
         </div>
     );
 };
--- src/redux/index.js
+++ src/redux/index.js
@@ -1,3 +1,7 @@
+import {createStore} from "redux";
+import {composeWithDevTools} from "redux-devtools-extension";
+import {reducer} from "./reducers";
+import thunk from "redux-thunk"
+import {applyMiddleware} from "redux";
 
-
-export const store = createStore()
+export const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)))
